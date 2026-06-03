441053916ad3f447585dd61245f4b2799462acff
Cyber Pnatera <a.xusanjonov@uztmk.uz>
2026-06-03 22:20:58 +0500

    Update Options layout

--- Assets/Scripts/Activitys/OptionsActivity.cs
+++ Assets/Scripts/Activitys/OptionsActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Assets.Scripts.UI;
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 using static Assets.Scripts.GameOptions;
 
 namespace Assets.Scripts.Activitys
@@ -13,8 +12,6 @@ namespace Assets.Scripts.Activitys
         [SerializeField] private ToggleGroupForEnum<SizesOfSquare> m_SizesOfSquare;
         [SerializeField] private ToggleGroupForEnum<GameTypes> m_GameTypes;
         [SerializeField] private ToggleGroupForEnum<GameLevels> m_GameLevels;
-        [SerializeField] private Button m_StartGameButton;
-        [SerializeField] private Button m_BackButton;
 
         private GameOptions mGameOptions;
         private Type mStartingActivity;
@@ -40,29 +37,17 @@ namespace Assets.Scripts.Activitys
             { if (m_GameTypes.Toggles.TryGetValue(mGameOptions.GameType, out ToggleForEnum<GameTypes> toggle) && toggle) toggle.isOn = true; }
             { if (m_GameLevels.Toggles.TryGetValue(mGameOptions.GameLevel, out ToggleForEnum<GameLevels> toggle) && toggle) toggle.isOn = true; }
 
-            m_SizesOfSquare.OnChangeEnumValue.AddListener(selectedSizeOfSquare => {
-                mGameOptions.SizeOfSquar = selectedSizeOfSquare;
-                StartGameInteractable();
-            });
+            m_SizesOfSquare.OnChangeEnumValue.AddListener(selectedSizeOfSquare =>
+                mGameOptions.SizeOfSquar = selectedSizeOfSquare);
 
-            m_GameTypes.OnChangeEnumValue.AddListener(selectedGameType => {
-                mGameOptions.GameType = selectedGameType;
-                StartGameInteractable();
-            });
+            m_GameTypes.OnChangeEnumValue.AddListener(selectedGameType =>
+                mGameOptions.GameType = selectedGameType);
 
-            m_GameLevels.OnChangeEnumValue.AddListener(selectedLevel => {
-                mGameOptions.GameLevel = selectedLevel;
-                StartGameInteractable();
-            });
-
-            m_StartGameButton.onClick.AddListener(() => StartActivity(mStartingActivity));
-
-            m_BackButton.onClick.AddListener(() => OnBackPressed());
-
-            StartGameInteractable();
+            m_GameLevels.OnChangeEnumValue.AddListener(selectedLevel =>
+                mGameOptions.GameLevel = selectedLevel);
         }
 
-        private void StartGameInteractable() =>
-            m_StartGameButton.interactable = true /*ACTIVITY_TYPE != ActivitesID.Instance.GetId<MultiplayerGameActivity>() || mGameOptions.RecordData.Sum != 0*/;
+        public void OnStartActivity_Click() =>
+            StartActivity(mStartingActivity);
     }
 }
